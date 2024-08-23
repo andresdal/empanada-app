cb479f9ad8d4eab99a62049439603da612485b13
andresdal <andres.dalesssandro@gmail.com>
2024-08-23 14:05:08 -0300

    crear vista

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <EmpanadaApp />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmpanadaApp from './components/EmpanadaApp.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EmpanadaApp
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/EmpanadaApp.vue
@@ -0,0 +1,55 @@
+<template>
+    <div id="app">
+      <h1>Empanada Tracker</h1>
+      <input v-model="newEmpanada" placeholder="Nuevo sabor de empanada" />
+      <button @click="addEmpanada">Agregar Empanada</button>
+      <ul>
+        <li v-for="(empanada, index) in empanadas" :key="index">
+          {{ empanada.name }}: {{ empanada.quantity }}
+          <button @click="increment(index)">+</button>
+          <button @click="decrement(index)">-</button>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'EmpanadaApp',
+    data() {
+      return {
+        newEmpanada: '',
+        empanadas: []
+      };
+    },
+    methods: {
+      addEmpanada() {
+        if (this.newEmpanada.trim() !== '') {
+          this.empanadas.push({ name: this.newEmpanada, quantity: 0 });
+          this.newEmpanada = '';
+        }
+      },
+      increment(index) {
+        this.empanadas[index].quantity++;
+      },
+      decrement(index) {
+        if (this.empanadas[index].quantity > 0) {
+          this.empanadas[index].quantity--;
+        }
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  
+  button {
+    margin-left: 10px;
+  }
+  </style>
